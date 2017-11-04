c99179e2cde040b5b3784f474c3f61ea2c788f65
Seena Burns <hello@seenaburns.com>
2017-11-04 10:32:37 -0700

    Create frameless window

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function error(e) {
 function createWindow () {
 
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1000, height: 600, titleBarStyle: 'hiddenInset'})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
