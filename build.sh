7b8f6b1e42529caaf32eecf0a82a3ddfa34020d5
Seena Burns <hello@seenaburns.com>
2018-12-19 19:13:50 -0800

    Update build/deps to install sqlite3 from source

--- build.sh
+++ build.sh
@@ -4,4 +4,17 @@ set -euo pipefail
 
 mkdir -p build
 
-cat src/css/main.styl | $(npm bin)/stylus > build/style.css
+echo "⚡  INSTALLING NATIVE DEPENDENCIES FROM SOURCE"
+if [ ! -d "$(npm root)/sqlite3/lib/binding/electron-v4.0-darwin-x64/" ];
+then
+    echo "⚡  INSTALLING SQLITE3 FROM SOURCE FOR ELECTRON"
+    npm install sqlite3 --build-from-source --runtime=electron --target='4.0.0-beta.7' --dist-url=https://atom.io/download/electron
+else
+    echo "⚡  SQLITE3 ALREADY INSTALLED"
+fi
+
+echo "⚡  BUILDING CSS"
+cat src/css/main.styl | npx stylus > build/style.css
+
+echo "⚡  RUNNING WEBPACK"
+npx webpack
