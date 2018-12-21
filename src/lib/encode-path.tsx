d2dc81ad343c599a1cf9b75a80713ff1f624dc85
Seena Burns <hello@seenaburns.com>
2018-12-20 16:33:27 -0800

    Enocde # in encodePath

--- src/lib/encode-path.tsx
+++ src/lib/encode-path.tsx
@@ -1,5 +1,5 @@
 export default function encodePath(path: string): string {
   const uri = encodeURI(path);
-  // Encode all ?
-  return uri.replace(/\?/g, "%3F");
+  // Encode all ?, #
+  return uri.replace(/\?/g, "%3F").replace(/\#/g, "%23");
 }
