f7839f87d7cac37bfa8431ce7e9695163f6d8f1d
Seena Burns <hello@seenaburns.com>
2019-01-13 18:39:14 -0800

    Unencode backlashes for windows paths

--- src/lib/encode-path.tsx
+++ src/lib/encode-path.tsx
@@ -1,5 +1,9 @@
 export default function encodePath(path: string): string {
   const uri = encodeURI(path);
   // Encode all ?, #
-  return uri.replace(/\?/g, "%3F").replace(/\#/g, "%23");
+  // Unencode backslashes for windows paths
+  return uri
+    .replace(/\?/g, "%3F")
+    .replace(/\#/g, "%23")
+    .replace(/%5C/g, "\\");
 }
